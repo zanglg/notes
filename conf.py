fbfb6cf0ddebfe93f64a960f04e2b10ca4f9bbb0
Zang Leigang <zang.lg@outlook.com>
2022-11-04 21:18:42 +0800

    setup sphinx project

---
+++ conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Notes'
+copyright = '2022, Zang Leigang'
+author = 'Zang Leigang'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx.ext.graphviz',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_venv', '_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_title = 'Notes'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,11 @@
+.. Notes documentation master file, created by
+   sphinx-quickstart on Fri Nov  4 20:36:41 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Notes
+=====
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
