09c0fe28129b5d04e7776076fb27d19217d91bf2
Zang Leigang <zang.lg@outlook.com>
2022-11-07 20:34:07 +0800

    set graphviz output format to svg

--- conf.py
+++ conf.py
@@ -21,7 +21,9 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_venv', '_build', 'Thumbs.db', '.DS_Store']
 
+# Extensions configuration
 
+graphviz_output_format = 'svg'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
